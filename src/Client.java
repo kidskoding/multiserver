5ab109cd89da8ca73559848ff84a726d4f00f119
Anirudh Konidala <kidskodingclub@gmail.com>
2024-12-24 18:13:37 -0600

    Added a chat communication system via Server and Client

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
@@ -16,5 +20,53 @@ public class Client {
         Socket socket = new Socket(theirIP, theirPort);
 
         System.out.println("Successfully joined the server!");
+
+        Thread talkThread = new Thread(new TalkThread(socket));
+        Thread listenThread = new Thread(new ListenThread(socket));
+        talkThread.start();
+        listenThread.start();
+    }
+
+    private static class TalkThread implements Runnable {
+        private Socket socket;
+        public TalkThread(Socket socket) {
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            try {
+                System.out.println("Talk thread started!");
+                OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream());
+                PrintWriter pw = new PrintWriter(osw, true);
+                Scanner scan  = new Scanner(System.in);
+                while(!socket.isClosed()) {
+                    String message = scan.nextLine();
+                    pw.println(message);
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static class ListenThread implements Runnable {
+        private Socket socket;
+        public ListenThread(Socket socket) {
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            try {
+                System.out.println("Listen thread started!");
+                InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+                BufferedReader br = new BufferedReader(isr);
+                while(!socket.isClosed()) {
+                    String message = br.readLine();
+                    System.out.println(message);
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) throws Exception {
@@ -16,5 +21,53 @@ public class Server {
         String theirIP = socket.getInetAddress().getHostAddress();
         int theirPort = socket.getPort();
         System.out.println("Client Info is " + theirIP + ":" + theirPort);
+
+        Thread talkThread = new Thread(new TalkThread(socket));
+        Thread listenThread = new Thread(new ListenThread(socket));
+        talkThread.start();
+        listenThread.start();
+    }
+
+    private static class TalkThread implements Runnable {
+        private Socket socket;
+        public TalkThread(Socket socket) {
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            try {
+                System.out.println("Talk thread started!");
+                OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream());
+                PrintWriter pw = new PrintWriter(osw, true);
+                Scanner scan  = new Scanner(System.in);
+                while(!socket.isClosed()) {
+                    String message = scan.nextLine();
+                    pw.println(message);
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static class ListenThread implements Runnable {
+        private Socket socket;
+        public ListenThread(Socket socket) {
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            try {
+                System.out.println("Listen thread started!");
+                InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+                BufferedReader br = new BufferedReader(isr);
+                while(!socket.isClosed()) {
+                    String message = br.readLine();
+                    System.out.println(message);
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
